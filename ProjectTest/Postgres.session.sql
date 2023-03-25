c7792428855638eb9dede1138d0805bbf377d90e
Emiliano Jimenez Guadarrama <emilianojg38@gmail.com>
2023-03-25 19:57:23 -0400

    Updates tables (Payment included)
    Adds  NOT NULL requirement to  Reservation (client and hotel id's).
    Changes the spelling of Location and adds attributes.
    Adds a payment table

--- ProjectTest/Postgres.session.sql
+++ ProjectTest/Postgres.session.sql
@@ -1,5 +1,11 @@
 SET search_path = 'SAILORS';
-CREATE TABLE capacity(
-    size VARCHAR(20)
-    PRIMARY KEY (size)
+CREATE TABLE payment(
+    payment_ID int NOT NULL,
+    amount FLOAT,
+    method VARCHAR(25),
+    client_ID int NOT NULL,
+    FOREIGN KEY (client_ID) REFERENCES client,
+    location_ID int NOT NULL,
+    FOREIGN KEY (location_ID) REFERENCES location,
+    PRIMARY KEY(payment_ID)
 );
