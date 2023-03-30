f0cebef0e4fd2403e85f0c72b9bcbcd79c91b7ff
Emiliano Jimenez Guadarrama <emilianojg38@gmail.com>
2023-03-29 22:43:29 -0400

    Adds View number 2
    Adds an sql of view number 2. This view lets you see all the rooms in a hotel with information that a client would probaly wnat to see, but the rooms are group by their capacity.
    It is important to note that a Where clause should be added to check that the rooms that this view shows is available.

--- ProjectTest/Postgres.session.sql
+++ ProjectTest/Postgres.session.sql
@@ -1,11 +1,3 @@
 SET search_path = 'SAILORS';
-CREATE TABLE payment(
-    payment_ID int NOT NULL,
-    amount FLOAT,
-    method VARCHAR(25),
-    client_ID int NOT NULL,
-    FOREIGN KEY (client_ID) REFERENCES client,
-    location_ID int NOT NULL,
-    FOREIGN KEY (location_ID) REFERENCES location,
-    PRIMARY KEY(payment_ID)
-);
+
+SELECT * from room Natural join hotel natural join capacity 
