350f73065a5909254f28b8566d3849bdd13dec63
kvn154 <ktawa043@uottawa.ca>
2023-03-31 17:20:52 -0400

    update

--- Project_code/eHotels/app/models.py
+++ Project_code/eHotels/app/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from datetime import datetime
 
-# Create your models here.
+# Create your models here..
 class User(AbstractUser):
     pass
 
